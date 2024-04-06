46ab8f01ef44c7b0c4e88ab3a78d3ba146b7ea52
Isaque Mattos <isaque.mattos@git.com>
2024-04-06 02:28:08 -0300

    Projeto de lista contígua

---
+++ ProjetoLista/src/main/java/listaContigua/Item.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaContigua;
+
+public class Item {
+    private int codigo;
+    private String nome;
+    
+    public Item(){
+        this.codigo = 0;
+        this.nome = null;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "codigo=" + codigo + ", nome=" + nome + '}';
+    }
+}
---
+++ ProjetoLista/src/main/java/listaContigua/ListaContigua.java
@@ -0,0 +1,59 @@
+package listaContigua;
+
+public class ListaContigua {
+    private Item[] vetorItem;
+    private int quant;
+    
+    public ListaContigua(){
+        this.vetorItem = new Item[10];
+        this.quant = 0;
+    }
+    
+    public ListaContigua(int tamanho){
+        this.vetorItem = new Item[tamanho];
+        this.quant = 0;
+    }
+    
+    public int tamanhoLista(){
+        return this.vetorItem.length;
+    }
+    
+    public int quantidadeElementos(){
+        return this.quant;
+    }
+    
+    public boolean eVazio(){
+        return this.quant == 0;
+    }
+    
+    public boolean eCheio(){
+        return this.quant == this.vetorItem.length;
+    }
+    
+    public Item getItem(int pos){
+        if(pos < 0 || pos > this.vetorItem.length-1)
+            return null;
+        
+        return this.vetorItem[pos];
+    }
+    
+    public void aumentaLista(){
+        Item[] novoVetor = new Item[this.vetorItem.length + (this.vetorItem.length / 2)];
+            
+        for(int i = 0; i < this.vetorItem.length; i++) {
+            novoVetor[i] = this.vetorItem[i];
+        }
+
+        this.vetorItem = novoVetor;
+    }
+    
+    public boolean insere(int pos, Item item){
+        aumentaLista();
+        
+        if(pos < 0 || pos > this.quant){
+            return false;
+        }
+        
+        return true;
+    }
+}
